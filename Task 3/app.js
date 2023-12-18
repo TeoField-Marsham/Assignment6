4e8654731da19ce1a7d4d95790e45a3eea115a7c
TeoField-Marsham <126777160+TeoField-Marsham@users.noreply.github.com>
2023-12-18 19:23:41 +0100

    updated app.js and index.html for task 3

--- Task 3/app.js
+++ Task 3/app.js
@@ -10,7 +10,7 @@ const io = new Server(httpServer);
 const QueryEngine = require('@comunica/query-sparql').QueryEngine;
 const engine = new QueryEngine();
 
-const port = 5001;
+const port = 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -32,42 +32,45 @@ app.get('/movies', (req, res) => {
 });
 
 async function handle_query(input_url) {
-    let movie_urls = []; // List of movies in the pod
+    let movie_urls = [];
 
-    // Query to get movie URLs
+    // First query to fetch the URLs of all items in the container
     await engine.queryBindings(`
-        PREFIX schema: <http://schema.org/>
+        PREFIX ldp: <http://www.w3.org/ns/ldp#>
         SELECT ?movie WHERE {
-            ?movie a schema:Movie .
+            ?container ldp:contains ?movie .
         }
     `, { sources: [input_url] }).then(async function (bindingsStream) {
+        // Process each binding and push movie URLs to the array
         bindingsStream.on('data', function (data) {
             movie_urls.push(data.get('movie').value);
         });
 
-        // Wait for the first query to complete
+        // Wait for the stream of data to end
         await new Promise(resolve => bindingsStream.on('end', resolve));
 
-        // Query each movie URL for details
-        for (const movie_url of movie_urls) {
-            await engine.queryBindings(`
-                PREFIX schema: <http://schema.org/>
-                SELECT ?name ?image WHERE {
-                    <${movie_url}> schema:name ?name;
-                                  schema:image ?image .
-                }
-            `, { sources: [movie_url] }).then(function (bindingsStream) {
-                bindingsStream.on('data', function (data) {
-                    let obj = {
-                        "name": data.get('name').value,
-                        "image": data.get('image').value
-                    };
-                    io.emit('update', {'message': obj});
-                });
+        // Second query to fetch the name and image of each movie
+        await engine.queryBindings(`
+            PREFIX schema: <https://schema.org/>
+            SELECT ?name ?image WHERE {
+                ?movie schema:name ?name;
+                       schema:image ?image .
+            }
+        `, { sources: movie_urls }).then(function (bindingsStream) {
+            console.log("Movie URLs: ", movie_urls); // Log the fetched movie URLs
+
+            // Process each binding to extract movie details
+            bindingsStream.on('data', function (data) {
+                let obj = {
+                    "name": data.get('name').value,
+                    "image": data.get('image').value
+                };
+                io.emit('update', {'message': obj}); // Emit the movie details to the client
             });
-        }
+        });
     });
 }
+
 
 httpServer.listen(port, () => {
     console.log("Server is running on port " + port);
